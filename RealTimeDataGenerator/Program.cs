7218a4ab9ec119ec87a0c69e3c850deabe7c2f40
Magda <m.breitenauer@outlook.com>
2024-06-23 21:26:09 +0200

    add files

---
+++ RealTimeDataGenerator/Program.cs
@@ -0,0 +1,27 @@
+using RealTimeDataGenerator;
+using RealTimeDataGenerator.models;
+using Serilog;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(ConfigureAppConfig)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<Worker>();
+        var serviceConfig = context.Configuration.Get<WorkerConfig>();
+        services.AddSingleton<WorkerConfig>(serviceConfig);
+    })
+    .UseSerilog((hostingContext, loggerConfig) => loggerConfig.ReadFrom.Configuration(hostingContext.Configuration))
+    .UseConsoleLifetime()
+    .Build();
+
+void ConfigureAppConfig(HostBuilderContext hostingContext, IConfigurationBuilder builder)
+{
+    builder.SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.json", false, true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+        .AddEnvironmentVariables();
+
+    if (!hostingContext.HostingEnvironment.IsDevelopment()) return;
+}
+
+await host.RunAsync();
